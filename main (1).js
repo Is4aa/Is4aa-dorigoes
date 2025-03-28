5cb50ad11fc7d8747ef37d941cec5dcf0edfe514
Is4aa <167428760+Is4aa@users.noreply.github.com>
2025-03-28 12:01:33 -0300

    Update main.js

--- main (1).js
+++ main (1).js
@@ -1,16 +1,25 @@
-const botoes = document.querySelectorAll(«.botao»);
-console.log(botoes)
-for (let i=0; i < botões.length; i++) { console.log(i); }
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function() {
-    botoes[i].classList.add(«ativo»);
-    }
-    }
-    const botoes = document.querySelectorAll(«.botao»);
-for(let i=0;i <botoes.length;i++){ botoes[i].onclick = function() {
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo")
+const abas = document.querySelectorAll(".aba-conteudo");
 
-for(let j=0;j<botoes.length;j++) { botoes[j].classList.remove(«ativo»);
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+        // Remove a classe "ativo" de todos os botÃµes e abas
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+            abas[j].classList.remove("ativo");
+        }
+
+// Adiciona a classe "ativo" apenas ao botÃ£o e Ã  aba correspondente
+botoes[i].classList.add("ativo");
+
+textos[i].classList.add("ativo");
+textos[j].classList.remove("ativo");
+
+abas[i].classList.add("ativo");
+};
 }
-botoes[i].classList.add(«ativo»);
-}
-}
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2025-10-05T00:00:00");
+
+contadores[0].textContent = tempoObjetivo1;
